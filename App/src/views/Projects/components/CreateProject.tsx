e55894d96df3f2b7f3bf7bba834dfcef3dd1ca46
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2021-07-17 12:09:54 -0700

    Added Goal parameter for project creation

--- App/src/views/Projects/components/CreateProject.tsx
+++ App/src/views/Projects/components/CreateProject.tsx
@@ -8,22 +8,26 @@ import { useForm } from 'react-hook-form'
 interface FormProps {
     name: string,
     category: string,
+    goal: string,
     setName: (name:string) => void,
     setCategory: (category:string) => void,
+    setGoal: (goal:string) => void,
     onSubmit: () => void,
 }
 
 const Form: React.FC<FormProps> = ({
     name,
     category,
+    goal,
     setName,
     setCategory,
+    setGoal,
     onSubmit
-}) => {    
+}) => {
 
     return (
         <FormWrap>
-            <Card style="glass">            
+            <Card style="glass">
                 <form>
                     <InputGrp>
                         <label>Project Name</label>
@@ -33,38 +37,50 @@ const Form: React.FC<FormProps> = ({
                                 autoFocus
                                 required
                                 value={name}
-                                onChange={(e) => {setName(e.target.value)}}                            
+                                onChange={(e) => {setName(e.target.value)}}
                             />
-                        </InputWrap>                    
+                        </InputWrap>
                     </InputGrp>
 
                     <InputGrp>
                         <label>Category Name</label>
                         <InputWrap>
                             <input
-                                type="text"                    
+                                type="text"
                                 required
                                 value={category}
                                 onChange={(e) => {setCategory(e.target.value)}}
                             />
-                        </InputWrap>                    
-                    </InputGrp>  
+                        </InputWrap>
+                    </InputGrp>
+
+                    <InputGrp>
+                        <label>Contribution Goal</label>
+                        <InputWrap>
+                            <input
+                                type="text"
+                                required
+                                value={goal}
+                                onChange={(e) => {setGoal(e.target.value)}}
+                            />
+                        </InputWrap>
+                    </InputGrp>
 
                     <ButtonGrp>
                         <br/>
-                        <Button 
-                            onClick={onSubmit} 
+                        <Button
+                            onClick={onSubmit}
                             variant="secondary">Create Project</Button>
                     </ButtonGrp>
-                </form>            
+                </form>
             </Card>
         </FormWrap>
     )
 }
 
-const FormWrap = styled.div`       
+const FormWrap = styled.div`
     max-width: 450px;
-    margin: 100px auto;    
+    margin: 100px auto;
     color: ${props => props.theme.color.white};
 `;
 
@@ -73,7 +89,7 @@ const Toggle = styled.div`
     cursor: default;
 
     span {
-        cursor: pointer;        
+        cursor: pointer;
         font-weight: bold;
         color: ${props => props.theme.color.purple[400]};
 
@@ -88,12 +104,12 @@ const InputGrp = styled.div`
     text-align: left;
 
     label {
-        color: ${props => props.theme.color.white};        
+        color: ${props => props.theme.color.white};
         display: block;
         font-weight: bold;
         margin-bottom: 5px;
-    } 
-    
+    }
+
     p.errorMsg {
         color: ${props => props.theme.color.red[200]};
     }
@@ -108,7 +124,7 @@ const InputWrap = styled.div`
     backdrop-filter: blur(5px);
     -webkit-backdrop-filter: blur(5px);
     border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-    border-right: 1px solid rgba(255, 255, 255, 0.1);      
+    border-right: 1px solid rgba(255, 255, 255, 0.1);
 
     input {
         background: none;
@@ -124,7 +140,7 @@ const InputWrap = styled.div`
 `;
 
 const ButtonGrp = styled.div`
-  
+
 `;
 
 export default Form
