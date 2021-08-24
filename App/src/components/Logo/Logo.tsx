b45b7ce5040ffb39a619b8b18af0171cef7f4536
Jacob Speirs <Jacob@c3.local>
2021-08-23 21:20:35 -0700

    Changed logo in nav bar from Icewater to Tributary
    Changed logo in nav bar from Icewater to Tributary

--- App/src/components/Logo/Logo.tsx
+++ App/src/components/Logo/Logo.tsx
@@ -2,29 +2,38 @@ import React from 'react';
 import styled from 'styled-components';
 
 import farmer from '../../assets/img/farmer.png';
-import icewaterLogo from '../../assets/img/icewater_logo_color.svg';
+import icewaterLogo from '../../assets/img/TributaryDelta.png';
 
 const Logo: React.FC = () => {
   return (
     <StyledLogo>      
       <StyledLink href="/">
-        <img src={icewaterLogo} height="32" style={{ marginTop: -4 }} />
+        <img src={icewaterLogo} height="32"/>
+        <BrandName>Tributary</BrandName>
       </StyledLink>
     </StyledLogo>
   );
 };
 
 const StyledLogo = styled.div`
-  align-items: center;
-  display: flex;
+  
 `;
 
 const StyledLink = styled.a`
+  display: flex;
+  align-items: center;
   color: ${(props) => props.theme.color.grey[900]};
   text-decoration: none;
   font-size: 18px;
   font-weight: 700;
   margin-left: ${(props) => props.theme.spacing[2]}px;
 `;
+
+const BrandName = styled.span`
+  color: #fff;
+  margin-left: 10px;
+`;
+
+
 
 export default Logo;
